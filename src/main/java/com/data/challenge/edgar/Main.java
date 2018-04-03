b62016ce49c1793181313aa89ad84f7ca11843f4
Chandravadan <s.chandravadan@gmail.com>
2018-04-03 08:45:36 +0530

    Some changes, create output file

--- src/main/java/com/data/challenge/edgar/Main.java
+++ src/main/java/com/data/challenge/edgar/Main.java
@@ -2,16 +2,24 @@ package com.data.challenge.edgar;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Contents of input file: ");
 
         Files.lines(Paths.get(args[0])).forEach(System.out::println);
+        List<String> lines = Files.lines(Paths.get(args[0])).collect(Collectors.toList());
+
 
         System.out.println("Inactivity period : " + Files.lines(Paths.get(args[1]))
                 .mapToInt(Integer::parseInt)
                 .findFirst()
                 .orElse(-1));
+
+        Files.createFile(Paths.get(args[2]));
+
+        Files.write(Paths.get(args[2]), lines);
     }
 }
